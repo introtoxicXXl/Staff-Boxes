150fcc837a19e8e393fef55da80457a2d8dc49d4
introtoxicXXl <minhajulabedin648@gmail.com>
2024-01-24 16:30:13 +0600

    firebase env added

--- src/Utilities/Firebase/Firebase.config.js
+++ src/Utilities/Firebase/Firebase.config.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAcTl-Om8JyeSbew0dlgyxwBY-qu7hjREg",
-  authDomain: "stuff-boxes.firebaseapp.com",
-  projectId: "stuff-boxes",
-  storageBucket: "stuff-boxes.appspot.com",
-  messagingSenderId: "823627435526",
-  appId: "1:823627435526:web:43e237565f8f5cf9407d87",
-  measurementId: "G-Y61GS5RHS0"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
+  measurementId:import.meta.env.VITE_MEASUREMENTID
 };
 
 const app = initializeApp(firebaseConfig);
